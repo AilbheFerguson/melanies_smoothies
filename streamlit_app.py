9b21b0da3daa4c1cb4409a15d23e9d8f59e0d53d
AilbheFerguson <169165071+AilbheFerguson@users.noreply.github.com>
2024-06-18 15:28:56 +0100

    Link_dataframe_to_ingredients.py
    Put dataframe creation in for loop on ingredients list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -29,6 +30,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_dv = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -45,8 +48,6 @@ if ingredients_list:
         
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
         
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_dv = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
     
